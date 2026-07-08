7498d687a032d721f9aab24bc440f7734c48e985
Tohir Mirxomitov <121395697+Mirxomitov@users.noreply.github.com>
2026-07-08 22:43:40 +0500

    add routes

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 from webob import Request, Response 
 
 class PyFremioApp:
+    def __init__(self) -> None:
+        self.routes = dict()
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
     
     def handle_request(self, request):
-        user_agent = request.environ.get("HTTP_USER_AGENT", "User agent not found")
-
         response = Response()
-        response.text =  f"Hello {user_agent}"
 
+        for path, ketmon in self.routes.items():
+            if path == request.path:
+                ketmon(request, response)
+                return response
+
+        self.default_response(response)
         return response
+        
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not found"
+
+    def route(self, path):
+        def teshavoy(ketmon):
+            self.routes[path] = ketmon
+            return ketmon
+
+        return teshavoy
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from app import PyFremioApp
 
 app = PyFremioApp()
+
+@app.route("/home")
+def home(request, response) -> None: 
+    response.text = "Hello from Home"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from About"
